8c053171b1f2c902fca3842c7585933e4bb07f08
zhiyingzzhou <zhiyingzzhou@gmail.com>
2017-05-06 19:54:32 +0800

    '完善任务报表页面'

--- src/components/task/table.jsx
+++ src/components/task/table.jsx
@@ -2,126 +2,51 @@ import React, {Component} from 'react';
 
 import {Table} from 'antd';
 
-const renderContent = (value, row, index) => {
-  const obj = {
-    children: value,
-    props: {},
-  };
-  if (index === 4) {
-    obj.props.colSpan = 0;
-  }
-  return obj;
-};
-
-const columns = [{
-  title: 'Name',
-  dataIndex: 'name',
-  render: (text, row, index) => {
-    if (index === 0) {
-      return {
-        children: <a href="#">{text}</a>,
-        props: {
-          rowSpan: 5,
-        },
-      }
-    }
-    return {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          props: {
-        rowSpan: 0,
-      },
-    };
-  },
-}, {
-  title: 'Age',
-  dataIndex: 'age',
-  // render: renderContent,
-}, {
-  title: 'Home phone',
-  colSpan: 2,
-  dataIndex: 'tel',
-  // render: (value, row, index) => {
-  //   const obj = {
-  //     children: value,
-  //     props: {},
-  //   };
-  //   if (index === 2) {
-  //     obj.props.rowSpan = 2;
-  //   }
-  //   // These two are merged into above cell
-  //   if (index === 3) {
-  //     obj.props.rowSpan = 0;
-  //   }
-  //   if (index === 4) {
-  //     obj.props.colSpan = 0;
-  //   }
-  //   return obj;
-  // },
-}, {
-  title: 'Phone',
-  colSpan: 0,
-  dataIndex: 'phone',
-  // render: renderContent,
-}, {
-  title: 'Address',
-  dataIndex: 'address',
-  // render: renderContent,
-}];
-
-const data = [{
-  key: '1',
-  name: 'John Brown',
-  age: 32,
-  tel: '0571-22098909',
-  phone: 18889898989,
-  address: 'New York No. 1 Lake Park',
-}, {
-  key: '2',
-  name: 'John Brown',
-  tel: '0571-22098333',
-  phone: 18889898888,
-  age: 42,
-  address: 'London No. 1 Lake Park',
-}, {
-  key: '3',
-  name: 'John Brown',
-  age: 32,
-  tel: '0575-22098909',
-  phone: 18900010002,
-  address: 'Sidney No. 1 Lake Park',
-}, {
-  key: '4',
-  name: 'John Brown',
-  age: 18,
-  tel: '0575-22098909',
-  phone: 18900010002,
-  address: 'London No. 2 Lake Park',
-}, {
-  key: '5',
-  name: 'John Brown',
-  age: 18,
-  tel: '0575-22098909',
-  phone: 18900010002,
-  address: 'Dublin No. 2 Lake Park',
-}];
+import columns from 'data/table-columns/task-table';
 
 export default class TableComponents extends Component {
 
+    state = {
+      data: []
+    }
+
     componentDidMount() {
       let data = {
+        organization: '51金融圈',
         name: '刘德华',
-        loginnum: 11,
-        resumenum: 11,
-        job: 11,
-        zhilian: 11,
-        focus: 20,
-        pending: 20,
-        cnt: 20,
-      }
+        loginnum: 100,
+        resumenum: 100,
+        "51job": 100,
+        zhilian: 100,
+        focus: 100,
+        pending: 100,
+        cnt: 100,
+        subscribe: 100,
+        interview: 100,
+        sinterview: 100,
+        induction: 100,
+        complete: 100
+       }
+       for(let i=0;i<7;i++){
+          this.state.data.push(data);
+       }
+       this.state.data.push({
+          key: Math.random(),
+          organization: '51金融圈',
+          name: '合计'
+       });
+       this.state.data.push({
+          key: Math.random(),
+          complete: '注：用户名为空的人，显示该用户的编号 ［数据来源日期 2014-05-23 至 2015-04-04］'
+       });
+       this.setState({
+         data: this.state.data
+       });
     }
 
     render() {
         return (
-            <Table columns={columns} dataSource={data} bordered />
+            <Table columns={columns} dataSource={this.state.data} bordered />
         );
     }
 }
---
+++ src/data/table-columns/task-table.js
@@ -0,0 +1,135 @@
+import React from 'react';
+
+const rednerContent = (text,record,index) => {
+    const obj = {
+        children: text,
+        props: {}
+    }
+    if(index === 8){
+        obj.props.colSpan = 0;
+    }
+    return obj;
+}
+
+module.exports = [
+    {
+        "title": "所属机构",
+        "dataIndex": "organization",
+        "key": "organization",
+        "width": 95,
+        "render": (text,record,index) => {
+            if(index === 0){
+                return {
+                    children: text,
+                    props: {
+                        rowSpan: 9
+                    }
+                }
+            }
+            return {
+                props: {
+                    rowSpan: 0
+                }
+            }
+        }
+    },
+    {
+        "title": "姓名",
+        "dataIndex": "name",
+        "key": "name",
+        "width": 65,
+        "render": rednerContent
+    },
+    {
+        "title": "登陆次数",
+        "dataIndex": "loginnum", 
+        "key": "loginnum",
+        "width": 79,
+        "render": rednerContent
+    },
+    {
+        "title": "简历总数",
+        "dataIndex": "resumenum",
+        "key": "resumenum",
+        "width": 79,
+        "render": rednerContent
+    },
+    {
+        "title": "简历导入量(51job)",
+        "dataIndex": "51job",
+        "key": "51job",
+        "width": 137,
+        "render": rednerContent
+    },
+    {
+        "title": "简历导入量(智联)",
+        "dataIndex": "zhilian",
+        "key": "zhilian",
+        "width": 130,
+        "render": rednerContent
+    },
+    {
+        "title": "关注职位数",
+        "dataIndex": "focus",
+        "key": "focus",
+        "width": 92,
+        "render": rednerContent
+    },
+    {
+        "title": "待处理申请者",
+        "dataIndex": "pending",
+        "key": "pending",
+        "width": 105,
+        "render": rednerContent
+    },
+    {
+        "title": "申请",
+        "dataIndex": "cnt",
+        "key": "cnt",
+        "width": 65,
+        "render": rednerContent
+    },
+    {
+        "title": "预约",
+        "dataIndex": "subscribe",
+        "key": "subscribe",
+        "width": 61,
+        "render": rednerContent
+    },
+    {
+        "title": "面试",
+        "dataIndex": "interview",
+        "key": "interview",
+        "width": 61,
+        "render": rednerContent
+    },
+    {
+        "title": "复试",
+        "dataIndex": "sinterview",
+        "key": "sinterview",
+        "width": 61,
+        "render": rednerContent
+    },
+    {
+        "title": "入职",
+        "dataIndex": "induction",
+        "key": "induction",
+        "width": 61,
+        "render": rednerContent
+    },
+    {
+        "title": "已处理",
+        "dataIndex": "complete",
+        "key": "complete",
+        "render": (text,record,index) => {
+            const obj = {
+                children: text,
+                props: {}
+            }
+            if(index === 8){
+                obj.props.colSpan = 13;
+            }
+            return obj;
+        }
+    }
+]
