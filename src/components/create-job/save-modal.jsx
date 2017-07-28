af698bf23595ca16f2284377c81427462a1bfc1c
lollipop7 <juanlilu@yeah.net>
2017-07-28 15:27:56 +0800

    新建职位保存modal 静态 ---4

--- src/components/create-job/save-modal.jsx
+++ src/components/create-job/save-modal.jsx
@@ -14,18 +14,18 @@ class SaveModalComponent extends Component {
             <Modal
                 title="温馨提示"
                 visible = {saveModalVisible}
-                className = "saveModal"
+                className = "save-modal"
                 closable = {true}
-                cancelText = "返回职位管理"
                 okText = "保存"
+                cancelText = "返回职位管理"
                 onCancel = {() => hideSaveJobModal()}
                 style = {{width:800,height:600}}
             >
-                <Icon 
-                    type="check-circle-o"
-                />
-                <p>职位发布成功</p>
-                <p>该职位要求是否保存到最近发布职位以便重复使用？</p>
+                <div className="tick-text" style={{textAlign: "center"}}>
+                    <img src="./static/images/job/tick.png" alt="钩" className="tick"/>
+                    <p>职位发布成功</p>
+                    <p>该职位要求是否保存到最近发布职位以便重复使用？</p>
+                </div>
             </Modal>
         )
     }
