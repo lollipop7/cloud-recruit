5ef0e5a8f8993aa789f359090f463bba00f11a9c
yangdoubleming <yangdoubleming@163.com>
2017-09-25 18:22:12 +0800

    组织架构-部门管理

--- src/reducer/manage.js
+++ src/reducer/manage.js
@@ -17,7 +17,8 @@ import {
     GET_EMPLOYEE_QUALITY,
     GET_DEPARTMENT_LIST,
     SHOW_PERSONALMATERIAL_MODAL,
-    HIDE_PERSONALMATERIAL_MODAL
+    HIDE_PERSONALMATERIAL_MODAL,
+    GET_DEPARTMENT_STAFF
 } from 'constants/manage'; 
 
 const initialState = {
@@ -43,12 +44,13 @@ const initialState = {
     crewDetail: {},
     archivesData: {},
     archivesTableData:'1',
+    personalMaterialVisible:false,
     departmentList: {
         isLoading: false,
         list: [],
         count: 0
     },
-    personalMaterialVisible:false
+    departmentStaff: {}
 };
 
 export default function manage(state=initialState,actions){
@@ -90,7 +92,9 @@ export default function manage(state=initialState,actions){
         case SHOW_PERSONALMATERIAL_MODAL:
             return {...state,personalMaterialVisible:actions.personalMaterialVisible};
         case HIDE_PERSONALMATERIAL_MODAL:
-            return {...state,personalMaterialVisible:actions.personalMaterialVisible};          
+            return {...state,personalMaterialVisible:actions.personalMaterialVisible};
+        case GET_DEPARTMENT_STAFF:
+            return {...state,departmentStaff:actions.departmentStaff};          
         default:
             return state;
     }
