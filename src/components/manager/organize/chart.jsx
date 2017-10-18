6322b244548839999b34e36049cca04c0cea22ff
yangdoubleming <yangdoubleming@163.com>
2017-10-18 11:57:11 +0800

    部门管理优化

--- src/components/manager/organize/chart.jsx
+++ src/components/manager/organize/chart.jsx
@@ -5,21 +5,25 @@ import { connect } from 'react-redux';
 import * as Actions from 'actions';
 import {Link} from 'react-router';
 import './chart.css' 
+// loading
+import LoadingComponent from 'components/loading';
 // antd
 import { Modal, Select } from 'antd';
 const Option = Select.Option;
 
 class OrganizeChart extends Component {
   state = { 
+    isLoading:false
   }
   componentDidMount(){
     // 获取数据
     this.props.getOrganizeChart();
     
   }
-
+  componentShouldUpdate(){
+    return nextProps !== this.props || nextState !== this.state
+  }
   componentWillUpdate(nextProps,nextState) {
-    const {isLoading} = nextState;
     const {organize} = nextProps;
     if(nextProps !== this.props){
         var newOrg = JSON.parse(JSON.stringify(organize).replace(/list/g, "childrens"))
@@ -49,13 +53,34 @@ class OrganizeChart extends Component {
                 }
             });
         }
+        
+    }
+}
+componentDidUpdate(nextProps,nextState){
+    if(nextState != nextState){
+        this.setState({isLoading:true})
     }
 }
   
 render() {
+    const { isLoading } = this.state;
+    console.log(isLoading)
     return (
         <div id='jOrgChart'>
-            
+            {isLoading &&
+                <div style={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    zIndex: 1
+                }}>
+                    <LoadingComponent style={{
+                        position: 'absolute',
+                        width: '100%',
+                        backgroundColor: '#FFF'
+                    }} />
+                </div>
+            }
         </div>
     );
   }
